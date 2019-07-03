82691ea800990fa0d4b208e666e42655e98c16fa
Joslin, Ian <Ian.Joslin@cdk.com>
2019-07-03 09:04:09 -0700

    demo application: added pom dependencies and db connection

--- src/main/java/com/startech/demo/DemoApplication.java
+++ src/main/java/com/startech/demo/DemoApplication.java
@@ -1,7 +1,17 @@
 package com.startech.demo;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +20,38 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context) {
+		return args -> {
+			System.out.println("Inspect beans provided by Spring Boot: ");
+
+			String[] beanNames = context.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName: beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(@Autowired BasicDataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	public BasicDataSource dataSource()  throws URISyntaxException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username);
+		basicDataSource.setPassword(password);
+
+		return basicDataSource;
+	}
 }
