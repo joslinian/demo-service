5d3f60d22a4422dd05096411a3746a47aa8365fe
Joslin, Ian <Ian.Joslin@cdk.com>
2019-07-16 15:59:23 -0700

    resetting project to basic setup and dependencies

--- src/main/java/com/startech/demo/DemoApplication.java
+++ src/main/java/com/startech/demo/DemoApplication.java
@@ -1,12 +1,7 @@
 package com.startech.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -15,16 +10,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext context) {
-		return args -> {
-			System.out.println("Inspect beans provided by Spring Boot: ");
-
-			String[] beanNames = context.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName: beanNames) {
-				System.out.println(beanName);
-			}
-		};
-	}
 }
