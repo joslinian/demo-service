0741f22f2869347d2431d1c65838a078b8226a47
Joslin, Ian <Ian.Joslin@cdk.com>
2019-07-17 11:16:42 -0700

    added creation endpoints for studentm entities

--- src/main/java/com/startech/demo/DemoApplication.java
+++ src/main/java/com/startech/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.startech.demo;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -29,4 +32,21 @@ public class DemoApplication {
 
 		};
 	}
+
+//	@Bean
+//	public BasicDataSource dataSource() throws URISyntaxException {
+//		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//		String username = dbUri.getUserInfo().split(":")[0];
+//		String password = dbUri.getUserInfo().split(":")[1];
+//		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+//
+//		BasicDataSource basicDataSource = new BasicDataSource();
+//		basicDataSource.setUrl(dbUrl);
+//		basicDataSource.setUsername(username);
+//		basicDataSource.setPassword(password);
+//
+//		return basicDataSource;
+//	}
+
 }
--- src/main/java/com/startech/demo/controller/StudentController.java
+++ src/main/java/com/startech/demo/controller/StudentController.java
@@ -5,7 +5,11 @@ import com.startech.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,55 +25,23 @@ public class StudentController {
     public List<Student> getStudents() {
         return service.getAllStudents();
     }
-//
-//    @RequestMapping("/insert-into-leaderboard")
-//    public void insertIntoLeaderboard(@RequestParam(value = "name") String name, @RequestParam(value = "score") int score) {
-//        jdbcTemplate.execute("insert into leaderboard values('" + name + "', " + score + ")");
-//    }
-//
-//    @CrossOrigin
-//    @RequestMapping(value = "/get-table", method = RequestMethod.GET, produces = "application/json")
-//    public List<Map<String, Object>> getTable() {
-//        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from  player_info order by max_streak desc");
-//        return maps;
-//    }
-//
-//    @CrossOrigin
-//    @RequestMapping(value = "/store-username")
-//    public void storeUsername(@RequestParam(value = "name") String name) {
-//        jdbcTemplate.execute("insert into player_info values('" + name + "', 0, 0)");
-//    }
-//
-//    @CrossOrigin
-//    @RequestMapping("/up-streaks")
-//    public void upStreaks(@RequestParam(value = "name") String playerName) {
-//        int currStreak = jdbcTemplate.queryForInt("select current_streak from player_info WHERE name = '" + playerName + "'") + 1;
-//        int maxStreak = jdbcTemplate.queryForInt("select max_streak from player_info WHERE name = '" + playerName + "'");
-//        if (currStreak >= maxStreak) {
-//            jdbcTemplate.execute("UPDATE player_info SET current_streak =" + currStreak + ", max_streak = " + currStreak + "WHERE name = '" + playerName + "'");
-//        } else {
-//            jdbcTemplate.execute("UPDATE player_info SET current_streak =" + currStreak + " WHERE name = '" + playerName + "'");
-//        }
-//    }
-//
-//    @CrossOrigin
-//    @RequestMapping("/reset-current-streak")
-//    public void resetCurrStreak(@RequestParam(value = "name") String playerName) {
-//        jdbcTemplate.execute("UPDATE player_info SET current_streak = 0 WHERE name = '" + playerName + "'");
-//    }
-//
-//    @CrossOrigin
-//    @RequestMapping(value = "/get-current-streak", method = RequestMethod.GET, produces = "application/json")
-//    public int getCurrStreak(@RequestParam(value = "name") String playerName) {
-//        int currStreak = jdbcTemplate.queryForInt("select current_streak from player_info WHERE name = '" + playerName + "'");
-//        return currStreak;
-//    }
-//
-//    @CrossOrigin
-//    @RequestMapping(value = "/get-num-entries", method = RequestMethod.GET, produces = "application/json")
-//    public int getNumEntries() {
-//        int numEntries = jdbcTemplate.queryForInt("select count (name) from player_info");
-//        return numEntries;
-//    }
+
+    @GetMapping(path = "/student-by-id", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Student getStudentById(@RequestParam("id") final Long id) {
+        return service.getStudentById(id);
+    }
+
+    @PostMapping(path = "/add-account")
+    @ResponseBody
+    public void createStudentAccount(@RequestBody final Student student) {
+        service.save(student);
+    }
+
+    @PostMapping(path = "/add-students", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public void createStudentAccounts(@RequestBody final List<Student> students) {
+        service.saveAll(students);
+    }
 
 }
